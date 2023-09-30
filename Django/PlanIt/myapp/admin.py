5efc82123250b4a4d6f81045146b433109e8559e
abuthemonkey <145487980+abuthemonkey@users.noreply.github.com>
2023-09-30 16:16:56 -0700

    Add files via upload
    Created Models for Employee and Shifts
    Implemented Basic CRUD operations for employees and shifts as an admin

--- Django/PlanIt/myapp/admin.py
+++ Django/PlanIt/myapp/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import employee
+from .models import shift
+
+#Make display of models more reader-friendly by creating classes: 'tableName'Admin()
+class employeeAdmin(admin.ModelAdmin):
+    list_display = ("firstName", "lastName", "ID",)
+
+class shiftAdmin(admin.ModelAdmin):
+    list_display = ("shiftType", "date", "startTime", "endTime",)
 
 # Register your models here.
+admin.site.register(employee, employeeAdmin)
+admin.site.register(shift, shiftAdmin)
+
+
--- Django/PlanIt/myapp/models.py
+++ Django/PlanIt/myapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+#Table containing employee information
+class employee(models.Model):
+    ID = models.IntegerField(primary_key = True)
+    firstName = models.CharField(max_length=255)
+    lastName = models.CharField(max_length=255)
+    phoneNumber = models.IntegerField()
+    email = models.EmailField()
+    level = models.IntegerField()
+
+#Table containing information about shifts
+class shift(models.Model):
+    date = models.DateField()
+    startTime = models.TimeField(null = True)
+    endTime = models.TimeField(null = True)
+    shiftType = models.CharField(max_length = 255)
+    employeeID = models.ForeignKey(employee, null = True, blank = True, on_delete=models.SET_NULL)
+    
